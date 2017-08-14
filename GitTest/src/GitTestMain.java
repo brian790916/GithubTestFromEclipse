57a10833bed37b1eb5f0d15469e670491b8bc36e
Chien-po_Huang <Chien-po_Huang@PEG-I010020093.PEGA.CORP.PEGATRON>
2017-08-14 15:01:06 -0700

    commit after Github readme

--- GitTest/src/GitTestMain.java
+++ GitTest/src/GitTestMain.java
@@ -4,7 +4,7 @@ public class GitTestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a Git test.");
-
+		System.out.println("Made change after Github ReadMe.");
 	}
 
 }
