84835c151bbb542ce29b495df97135b15b785d3d
Chien-po_Huang <Chien-po_Huang@PEG-I010020093.PEGA.CORP.PEGATRON>
2017-08-14 19:34:54 -0700

    new start

--- GitTest/src/GitTestMain.java
+++ GitTest/src/GitTestMain.java
@@ -3,15 +3,7 @@ public class GitTestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a Git test.");
-		System.out.println("Made change after Github ReadMe.");
-		System.out.println("Create Brian branch.");
-		System.out.println("Continue Brian branch 1.");
-//		System.out.println("Merge Brian branch.");
-		System.out.println("Merge Master branch.");
-		System.out.println("Continue Master.");
-		System.out.println("master: Commit again.");
-		System.out.println("branch: Start branch");
+		System.out.println("New start.");
 	}
 
 }
